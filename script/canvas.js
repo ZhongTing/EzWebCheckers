79915cf415db070b028e93292aa2d7d50eb6dcf2
gray62107 <gary62107@gmail.com.tw>
2013-10-19 19:10:43 +0800

    test canvas input

--- script/canvas.js
+++ script/canvas.js
@@ -20,8 +20,33 @@ function turnToLobbyLayer()
 {
     var lobbyLayer = new Kinetic.Layer();
     var logoutLabel = newLabel(0,0,'logout').on('click',function(){logout();});
-    var refreshRoomListLabel = newLabel(0,100,'refresh room list').on('click',function(){listRoomInfos();});
+    var refreshRoomListLabel = newLabel(0,50,'refresh room list').on('click',function(){listRoomInfos();});
+    var createRoomLabel = newLabel(0,100,'CreateRoom').on('click',function(){openCreateRoomLayer();});
     lobbyLayer.add(logoutLabel).add(refreshRoomListLabel);
     stage.removeChildren('.layer');
     stage.add(lobbyLayer);
+}
+function turnToRoomLayer()
+{
+    var roomLayer = new Kinetic.Layer();
+    stage.removeChildren('.layer');
+    stage.add(roomLayer);
+}
+function openCreateRoomLayer()
+{
+    var input = new CanvasInput({
+        canvas: document.getElementsByTagName('canvas'),
+        fontSize: 18,
+        fontFamily: 'Arial',
+        fontColor: '#212121',
+        fontWeight: 'bold',
+        width: 300,
+        padding: 8,
+        borderWidth: 1,
+        borderColor: '#000',
+        borderRadius: 3,
+        boxShadow: '1px 1px 0px #fff',
+        innerShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)',
+        placeHolder: 'Enter message here...'
+    });
 }
