3a1540f047b59b75299ffd7a9c7817fddbd5b808
Keming <believe75467@gmail.com>
2013-10-23 00:32:02 +0800

    旗子行走方向定義完成

--- script/chessBoard.js
+++ script/chessBoard.js
@@ -2,81 +2,28 @@ var chessBoardCenter = {x:393.5,y:207};
 var chessBoardGridEdge = 45;
 var chessPoints = getInitChessPoint();
 var userCheckerColors = ["red","yellow","green"];
+var moveDirection = ["up", "right", "rightdown", "down", "left", "leftup"];
 var selectedChecker;
+var point;
+
 function getInitChessPoint()
 {
-    var point = {};
-    point["0,0"] = {x:0,y:0};
-    point["0,1"] = {x:0,y:1};
-    point["0,2"] = {x:0,y:2};
-    point["0,-1"] = {x:0,y:-1};
-    point["0,-2"] = {x:0,y:-2};
+    point = {};
     
-    point["1,0"] = {x:1,y:0};
-    point["1,1"] = {x:1,y:1};
-    point["1,2"] = {x:1,y:2};
-    point["1,-1"] = {x:1,y:-1};
-    point["1,-2"] = {x:1,y:-2};
-    point["1,-3"] = {x:1,y:-3};
+    //定義黃色 player 1 所在之地, TipPoint{-2,4} move rightdown & left
+    setPlayerAndDomain({x:-2,y:4},"rightdown","left",1,1);
     
-    point["2,0"] = {x:2,y:0};
-    point["2,1"] = {x:2,y:1};
-    point["2,2"] = {x:2,y:2};
-    point["2,-1"] = {x:2,y:-1};
-    point["2,-2"] = {x:2,y:-2};
-    point["2,-3"] = {x:2,y:-3};
-    point["2,-4"] = {x:2,y:-4};
+    //定義紅色 player 0 , TipPoint{-2,-2} move up & rightdown
+    setPlayerAndDomain({x:-2,y:-2},"up","rightdown",0,0);
     
-    point["3,-1"] = {x:3,y:-1};
-    point["3,-2"] = {x:3,y:-2};
-    
-    point["4,-2"] = {x:4,y:-2};
-    
-    point["-1,0"] = {x:-1,y:0};
-    point["-1,-1"] = {x:-1,y:-1};
-    point["-1,-2"] = {x:-1,y:-2};
-    point["-1,1"] = {x:-1,y:1};
-    point["-1,2"] = {x:-1,y:2};
-    point["-1,3"] = {x:-1,y:3};
-    
-    point["-2,0"] = {x:-2,y:0};
-    point["-2,-1"] = {x:-2,y:-1};
-    point["-2,-2"] = {x:-2,y:-2};
-    point["-2,1"] = {x:-2,y:1};
-    point["-2,2"] = {x:-2,y:2};
-    point["-2,3"] = {x:-2,y:3};
-    point["-2,4"] = {x:-2,y:4};
-    
-    point["-3,1"] = {x:-3,y:1};
-    point["-3,2"] = {x:-3,y:2};
-    
-    point["-4,2"] = {x:-4,y:2};
-    
-    
-    point["-2,-2"].player = 0;
-    point["-2,-1"].player = 0;
-    point["-2,0"].player = 0;
-    point["-1,-2"].player = 0;
-    point["-1,-1"].player = 0;
-    point["0,-2"].player = 0;
-    point["0,2"].player = 1;
-    point["-1,2"].player = 1;
-    point["-2,2"].player = 1;
-    point["-1,3"].player = 1;
-    point["-2,3"].player = 1;
-    point["-2,4"].player = 1;
-    point["2,0"].player = 2;
-    point["2,-1"].player = 2;
-    point["2,-2"].player = 2;
-    point["3,-1"].player = 2;
-    point["3,-2"].player = 2;
-    point["4,-2"].player = 2;
-    
+    //定義綠色 player 2 , TipPoint{4,-2} move left & up
+    setPlayerAndDomain({x:4,y:-2},"left","up",2,2);
     return point;
 }
-function getChessPoint(x,y)
+
+function getChessPoint(point)
 {
-    return chessPoints[x+","+y];
+    return chessPoints[point.x+","+point.y];
 }
 
 function gridXyToXy(point)
@@ -103,6 +50,7 @@ function findNear(x,y)
     if(point.player>=0)return;
     point.computed = true;
 }
+
 function find_recursive(point)
 {
     var jumpPoint = [];
@@ -119,5 +67,56 @@ function find_recursive(point)
         if(jumpPoint[i].player>=0)continue;
         jumpPoint[i].computed = true;
         find_recursive(jumpPoint[i]);
+    }
+}
+
+function setPlayerAndDomain(TipPoint, leftDirection, rightDirection, playerNumber, domain)
+{
+    for(var i=0; i<3; i++)
+    {
+        for(var j=0; j<=i; j++)
+        {
+            var tempPoint = {x:TipPoint.x, y:TipPoint.y};
+            //move rightdown
+            for(var k=0; k<i; k++)
+            {
+                tempPoint = getMovePoint(leftDirection, tempPoint);
+            }
+            //left
+            for(var k=0; k<j; k++)
+            {
+                tempPoint = getMovePoint(rightDirection, tempPoint);
+            }
+            tempPoint.player = playerNumber;
+            tempPoint.domain = domain;
+            console.log(i + ", " + j + ": ");console.log(tempPoint);
+            point[tempPoint.x+","+tempPoint.y] = tempPoint;
+        }
+    }
+}
+
+function getMovePoint(direction, point, isJump)
+{
+    move = isJump == true ? 2 : 1;
+    switch(direction)
+    {
+        case 'up'://0
+            return {x:point.x,y:point.y+move};
+            break;
+        case 'down'://3
+            return {x:point.x,y:point.y-move};
+            break;
+        case 'left'://3
+            return {x:point.x-move,y:point.y};
+            break;
+        case 'right'://1
+            return {x:point.x+move,y:point.y};
+            break;
+        case 'leftup'://5
+            return {x:point.x-move,y:point.y+move};
+            break;
+        case 'rightdown'://2
+            return {x:point.x+move,y:point.y-move};
+            break;
     }
 }
